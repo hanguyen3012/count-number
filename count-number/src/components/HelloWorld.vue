6570bf519477147f2f87bed377f9dbec6459ed58
hanguyen3012 <hantt@dipro.vn>
2022-04-06 09:30:03 +0700

    [feature] count_num

---
+++ count-number/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="hello">
+      <h1>{{counter}}</h1>
+    </div>
+    <div>
+      <button @click="counter +=1">+</button>&nbsp;&nbsp;
+      <button @click="counter -=1">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data (){
+    return {
+      counter : 0,
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
